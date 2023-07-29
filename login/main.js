f079383cebe74131987c7ee27be8e67ce4883423
miguel190n <lokokkx3@gmail.com>
2023-07-29 04:39:35 -0300

    exclusão do arquivo JS não útil

--- login/main.js
+++
@@ -1,7 +0,0 @@
-// JavaScript:
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", function() {
-  // Redirecionar para a página HTML desejada
-  window.location.href = "../Página inicial/Página inicial/index.html";
-});
