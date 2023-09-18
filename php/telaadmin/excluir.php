c752e692dad1cb34d422a4e2e99cba79b5fd578d
miguel190n <lokokkx3@gmail.com>
2023-09-17 22:51:02 -0300

    add o excluir usuários

---
+++ php/telaadmin/excluir.php
@@ -0,0 +1,22 @@
+<?php
+
+    $iduser = $_GET['id'];
+
+    //CONEXÃO COM O BD
+    include_once("conexao.php");
+
+    //INSTRUÇÃO SQL
+    $stmt = "delte from usuarios where id = '$iduser';";
+
+    //EXECUTANDO O COMANDO SQL
+    if(mysqli_query($conn, $stmt)){
+        header("Location: telaadmin.php")
+    }else{
+        echo "Erro ao apagar o usuário.<br>".mysqli_error($conn);
+        echo "<br><a href='telaadmin.php'>Voltar</a>";
+    }
+
+    //FECHANDO O BD
+    mysqli_close($conn);
+
+?>
