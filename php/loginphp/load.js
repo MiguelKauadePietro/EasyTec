77b7253a96c934e40025d476f8540a6ad2ebc8ac
miguel190n <lokokkx3@gmail.com>
2023-10-23 01:48:03 -0300

    conSertando alguns detalhes na tela de cadastro e deletando alguns arquivos

--- php/loginphp/load.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    // Quando a página estiver completamente carregada
-    var loader = document.querySelector('.loader-wrapper');
-    var content = document.querySelector('.content');
-
-    // Esconde a tela de carregamento e mostra o conteúdo
-    loader.style.display = 'none';
-    content.style.display = 'block';
-}
