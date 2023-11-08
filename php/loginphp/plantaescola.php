a42dba1d8511b71ce9b5adbb7871ca1352607acf
miguel190n <lokokkx3@gmail.com>
2023-11-08 00:49:25 -0300

    formatando o botão de baixar a planta da escola

--- php/loginphp/plantaescola.php
+++ php/loginphp/plantaescola.php
@@ -17,6 +17,7 @@ $usuarioNome = $_SESSION['usuario_Nome'];
   <link rel="stylesheet" href="../../CSS/navresponsiva.css">
   <link rel="stylesheet" href="../../CSS/slide.css">
   <link rel="shortcut icon" href="../../imagens/EASYTEC.png" type="image/x-icon">
+
   <title>Página Inicial</title>
 </head>
 <body>
@@ -37,8 +38,8 @@ $usuarioNome = $_SESSION['usuario_Nome'];
 
 
   <main> 
-    <h3>Clique no botão abaixo para baixar a imagem da Planta da Escola</h3>
-    <button id="downloadButton">Baixar Imagem PNG</button>
+    <h3 class="msg_planta">Clique no botão abaixo para baixar a imagem da Planta da Escola</h3>
+    <button type="button" class="btn btn-primary btn-lg" id="downloadButton">Baixar Imagem PNG</button>
   </main>
 </header>
 
@@ -92,4 +93,7 @@ $usuarioNome = $_SESSION['usuario_Nome'];
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <script src="navbar.js"></script>
 <script src="baixarplanta.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 </html>
